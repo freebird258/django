33ce86057654c45214ac83ae55727254de92998f
Treble <1062609615@qq.com>
2016-11-23 14:40:12 +0800

    add handle urllib post

--- merchant/urls.py
+++ merchant/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^m_index/', 'merchant.views.m_index'),
+    url(r'^send_post/', 'merchant.views.send_post'),
+    url(r'^handle_post/', 'merchant.views.handle_post'),
     url(r'^$', 'merchant.views.m_index'),
 )
 
--- merchant/views.py
+++ merchant/views.py
@@ -2,10 +2,11 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.core.context_processors import csrf
 from django.core.cache import caches
+from django.views.decorators.csrf import csrf_exempt
 
-import memcache
+import memcache,json
 
 def m_index(request):
 	#mc = memcache.Client(['127.0.0.1:11211'])
@@ -19,3 +20,18 @@ def m_index(request):
 	context['a'] = 'aaaa'
 	print context['hello']
 	return render(request, 'merchant.html', context)
+
+def send_post(request):
+	return render(request, "investigate.html")
+
+@csrf_exempt 
+def handle_post(request):
+	ctx ={}
+#	ctx.update(csrf(request))
+	if request.method == 'POST':
+		data = eval(request.body)
+		ctx['rlt'] = data["staff"]
+	else:
+		ctx['rlt'] = "nothing"
+	return HttpResponse(ctx['rlt'])
+	#return HttpResponse(json.dumps(request.body),content_type="application/json")	
